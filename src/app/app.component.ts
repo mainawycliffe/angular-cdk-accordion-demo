67eb425e6776fb0fa51244c4bf484c8660606ea9
Maina Wycliffe <wmmaina7@gmail.com>
2022-06-17 01:01:55 +0300

    Convert to Standalone Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class AppComponent {
   title = 'angular-cdk-accordion-demo';
